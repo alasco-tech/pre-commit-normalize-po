ecb18626731fabc9a70f35abc61910b117dc9079
Lorenzo Peña <lorenzo.pena@alasco.de>
2023-02-22 10:58:12 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='normalize_po',
-    version='1.1.1',
+    version='1.1.2',
     install_requires=['polib ~= 1.1.0'],
     entry_points={
         'console_scripts': [
